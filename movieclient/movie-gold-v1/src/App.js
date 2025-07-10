ffddf15828ed46e76fa6e886ea3fe55d0106b33c
trader87 <ironbone98@gmail.com>
2025-07-10 15:46:31 +0700

    Update

--- movieclient/movie-gold-v1/src/App.js
+++ movieclient/movie-gold-v1/src/App.js
@@ -7,6 +7,7 @@ import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
 import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 
 function App() {
@@ -66,6 +67,7 @@ function App() {
           <Route path="/" element={<Home movies={movies} />}></Route>
           <Route path='/Trailer/:ytTrailerId' element={Trailer}></Route>
           <Route path='/Reviews/:movieId' element={<Reviews getMovieData ={getMovieData} movie ={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+          <Route path="*" element = {<NotFound/>}></Route>
         </Route>
       </Routes>
       
